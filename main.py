ffa0386986d94515ac9219b08914537759c94683
Robson Diógenes <robsonad07@gmail.com>
2026-08-12 16:50:19 -0300

    cors implemented

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Depends
+from fastapi.middleware.cors import CORSMiddleware
 import auth
 import events 
 from models import User, RoleEnum
@@ -10,6 +11,14 @@ app = FastAPI(
     title="Event and Ticketing Platform API",
     description="This is the API for the Event and Ticketing Platform, which allows users to create and manage events, sell tickets, and handle payments.",
     version="1.0.0",
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(auth.router)
