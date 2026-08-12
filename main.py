039c73354daa30e5a200259d1ab5d5dce96af38e
Robson Diógenes <robsonad07@gmail.com>
2026-08-11 21:06:46 -0300

    route /login impremented

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from fastapi import FastAPI
+import auth
 
 app = FastAPI(
     title="Event and Ticketing Platform API",
     description="This is the API for the Event and Ticketing Platform, which allows users to create and manage events, sell tickets, and handle payments.",
     version="1.0.0",
 )
+
+app.include_router(auth.router)
 
 @app.get("/health", tags=["Health"])
 def health_check():
