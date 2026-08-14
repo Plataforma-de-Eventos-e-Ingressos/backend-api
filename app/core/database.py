18d610387b9f501e68637bc230121699c6bcb042
Robson Diógenes <robsonad07@gmail.com>
2026-08-14 15:18:56 -0300

    database updated

--- app/core/database.py
+++ app/core/database.py
@@ -10,12 +10,18 @@ SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 if not SQLALCHEMY_DATABASE_URL:
     raise ValueError("CRÍTICO: A variável de ambiente DATABASE_URL não está configurada!")
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL,
-    pool_pre_ping=True, 
-    pool_size=10,        
-    max_overflow=20      
-)
+if SQLALCHEMY_DATABASE_URL.startswith("sqlite"):
+    engine = create_engine(
+        SQLALCHEMY_DATABASE_URL, 
+        connect_args={"check_same_thread": False}  
+    )
+else:
+    engine = create_engine(
+        SQLALCHEMY_DATABASE_URL,
+        pool_pre_ping=True,
+        pool_size=10,
+        max_overflow=20
+    )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
